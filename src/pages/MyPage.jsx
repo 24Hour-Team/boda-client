5d1acb4a8586fb2f6e48f35a0b10ad2a9eb2d2cf
EunHakKim <acky529@gmail.com>
2024-08-26 17:21:06 +0900

    BODA-46 feat: 지난 AI 추천 기능 연동

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/MyPage.module.css';
 import folder from '../assets/images/folder.png';
 
+const regionMap = {
+  SEOUL: '서울',
+  GANGNEUNG: '강릉',
+  SUWON: '수원',
+  GAPYEONG: '가평',
+  DAEJEON: '대전',
+  INCHEON: '인천',
+  BUSAN: '부산',
+  DAEGU: '대구',
+  CHUNCHEON: '춘천',
+  JEJU: '제주',
+  GYEONGJU: '경주',
+  GONGJU: '공주',
+  JEONJU: '전주',
+  GUNSAN: '군산',
+  GWANGJU: '광주',
+  SUNCHEON: '순천',
+  SOKCHO: '속초',
+  YANGYANG: '양양',
+  DANYANG: '단양',
+  YEOSU: '여수',
+  TAEAN: '태안'
+};
+
+const seasonMap = {
+  SPRING: '봄',
+  SUMMER: '여름',
+  FALL: '가을',
+  WINTER: '겨울'
+};
+
+const travelStyles = [
+  { left: '자연', right: '도시' },
+  { left: '숙박', right: '당일' },
+  { left: '새로운', right: '익숙한' },
+  { left: '편하고 비싼 숙소', right: '불편하고 저렴한 숙소' },
+  { left: '휴양휴식', right: '엑티비티' },
+  { left: '안 유명한', right: '유명한' },
+  { left: '계획', right: '상황' },
+  { left: '사진 중요', right: '사진 안 중요' }
+];
+
 const MyPage = () => {
+  const [recommendations, setRecommendations] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchRecommendations = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/v1/recommend/list/529acky@naver.com`); // 백엔드 API 경로에 맞게 수정
+        const data = await response.json();
+
+        // 최신 날짜가 위로 오도록 정렬
+        const sortedData = data.sort((a, b) => new Date(b.createdDateTime) - new Date(a.createdDateTime));
+        setRecommendations(sortedData);
+      } catch (error) {
+        console.error('Failed to fetch recommendations:', error);
+      }
+    };
+
+    fetchRecommendations();
+  }, []);
+
+  const handleItemClick = (id) => {
+    navigate(`/result/${id}`);
+  };
+
+  const renderTravelStyles = (recommendation) => {
+    return travelStyles.map((style, index) => (
+      recommendation[`travelStyle${index + 1}`] ? style.right : style.left
+    )).join(', ');
+  };
   return (
     <div className={styles.myPageContainer}>
       <div className={styles.pageTitle}>마이페이지</div>
@@ -13,12 +85,16 @@ const MyPage = () => {
           <div className={styles.userInfo}>
             <h3>유저 정보</h3>
             <div className={styles.profileSection}>
-              <img src="http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640" alt="profile" className={styles.profileImage} />
+              <img
+                src="http://t1.kakaocdn.net/account_images/default_profile.jpeg.twg.thumb.R640x640"
+                alt="profile"
+                className={styles.profileImage}
+              />
               <div className={styles.userDetails}>
                 <h2 className={styles.nickname}>은학</h2>
                 <div className={styles.additionalInfo}>
-                    <p>20대 남성</p>
-                    <p>529acky@naver.com</p>
+                  <p>20대 남성</p>
+                  <p>529acky@naver.com</p>
                 </div>
               </div>
             </div>
@@ -31,42 +107,42 @@ const MyPage = () => {
             </div>
             <div className={styles.bookmarkList}>
               <div className={styles.bookmarkItem}>
-                <img src={folder} alt="folder" className={styles.bookmarkImage}/>
+                <img src={folder} alt="folder" className={styles.bookmarkImage} />
                 <div className={styles.bookmarkInfo}>
                   <h4>서울 여행</h4>
                   <p>생성 날짜</p>
                 </div>
               </div>
               <div className={styles.bookmarkItem}>
-                <img src={folder} alt="folder" className={styles.bookmarkImage}/>
+                <img src={folder} alt="folder" className={styles.bookmarkImage} />
                 <div className={styles.bookmarkInfo}>
                   <h4>강릉 여행</h4>
                   <p>생성 날짜</p>
                 </div>
               </div>
               <div className={styles.bookmarkItem}>
-                <img src={folder} alt="folder" className={styles.bookmarkImage}/>
+                <img src={folder} alt="folder" className={styles.bookmarkImage} />
                 <div className={styles.bookmarkInfo}>
                   <h4>제주도 여행</h4>
                   <p>생성 날짜</p>
                 </div>
               </div>
               <div className={styles.bookmarkItem}>
-                <img src={folder} alt="folder" className={styles.bookmarkImage}/>
+                <img src={folder} alt="folder" className={styles.bookmarkImage} />
                 <div className={styles.bookmarkInfo}>
                   <h4>서울 여행</h4>
                   <p>생성 날짜</p>
                 </div>
               </div>
               <div className={styles.bookmarkItem}>
-                <img src={folder} alt="folder" className={styles.bookmarkImage}/>
+                <img src={folder} alt="folder" className={styles.bookmarkImage} />
                 <div className={styles.bookmarkInfo}>
                   <h4>강릉 여행</h4>
                   <p>생성 날짜</p>
                 </div>
               </div>
               <div className={styles.bookmarkItem}>
-                <img src={folder} alt="folder" className={styles.bookmarkImage}/>
+                <img src={folder} alt="folder" className={styles.bookmarkImage} />
                 <div className={styles.bookmarkInfo}>
                   <h4>제주도 여행</h4>
                   <p>생성 날짜</p>
@@ -79,36 +155,26 @@ const MyPage = () => {
         <div className={styles.recommendationSection}>
           <h3>지난 AI 추천</h3>
           <div className={styles.recommendationList}>
-            <div className={styles.recommendationItem}>
-              <h4>제주도 겨울 여행</h4>
-              <p>추천 날짜</p>
-              <p>선택된 항목: 자연, 당일, 새로운, 편하고 비싼 숙소, 휴양휴식</p>
-            </div>
-            <div className={styles.recommendationItem}>
-              <h4>서울 여름 여행</h4>
-              <p>추천 날짜</p>
-              <p>선택된 항목: 자연, 당일, 새로운, 편하고 비싼 숙소, 휴양휴식</p>
-            </div>
-            <div className={styles.recommendationItem}>
-              <h4>강릉 봄 여행</h4>
-              <p>추천 날짜</p>
-              <p>선택된 항목: 자연, 당일, 새로운, 편하고 비싼 숙소, 휴양휴식</p>
-            </div>
-            <div className={styles.recommendationItem}>
-              <h4>여수 가을 여행</h4>
-              <p>추천 날짜</p>
-              <p>선택된 항목: 자연, 당일, 새로운, 편하고 비싼 숙소, 휴양휴식</p>
-            </div>
-            <div className={styles.recommendationItem}>
-              <h4>강릉 봄 여행</h4>
-              <p>추천 날짜</p>
-              <p>선택된 항목: 자연, 당일, 새로운, 편하고 비싼 숙소, 휴양휴식</p>
-            </div>
-            <div className={styles.recommendationItem}>
-              <h4>여수 가을 여행</h4>
-              <p>추천 날짜</p>
-              <p>선택된 항목: 자연, 당일, 새로운, 편하고 비싼 숙소, 휴양휴식</p>
-            </div>
+            {recommendations.map((rec) => (
+              <div
+                key={rec.id}
+                className={styles.recommendationItem}
+                onClick={() => handleItemClick(rec.id)}
+              >
+                <div className={styles.recommendationHeader}>
+                  <h4 className={styles.recommendationTitle}>
+                    {regionMap[rec.regionClassification]} {seasonMap[rec.season]} 여행
+                  </h4>
+                  <p className={styles.recommendationDate}>
+                    {new Date(rec.createdDateTime).toLocaleDateString()}
+                  </p>
+                </div>
+                <p></p>
+                <div className={styles.recommendationDetails}>
+                  선택된 항목: {renderTravelStyles(rec)}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
